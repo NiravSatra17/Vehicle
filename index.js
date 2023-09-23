48899a095ff378bb5a44ddaa97667407fe723877
NiravSatra17 <116972781+NiravSatra17@users.noreply.github.com>
2023-09-23 16:16:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,183 +6,214 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const con = mysql.createConnection({
-	host : "localhost",
-	user : "root",
-	password : "abc456",
-	database : "vehicle"
-})
+const createConnection = async () => {
+  const con = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "abc456",
+    database: "vehicle",
+  });
+  return con;
+};
 
-app.post("/saveUser", (req, res) => {
-	let data = [ req.body.un, req.body.vType, req.body.vNum ];
-	let sql = "insert into users (email, vType, vNum) values(?, ?, ?)";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
+app.post("/saveUser", async (req, res) => {
+  try {
+    const con = await createConnection();
+    const data = [req.body.un, req.body.vType, req.body.vNum];
+    const sql = "INSERT INTO users (email, vType, vNum) VALUES (?, ?, ?)";
+    const [result] = await con.execute(sql, data);
+    con.end(); // Close the connection
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
-app.get("/readUser", (req, res) => {
-	const userEmail = req.query.un; // Access the 'un' parameter from the query string
+app.get("/readUser", async (req, res) => {
+  try {
+    const userEmail = req.query.un; // Access the 'un' parameter from the query string
+    const sql = "SELECT * FROM users WHERE email=?";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, [userEmail]);
+    
+    // Close the connection
+    con.end();
 
-	let sql = "SELECT * FROM users WHERE email=?";
-	con.query(sql, [userEmail], (err, result) => {
-		if (err) 	res.send(err);
-		else 		res.send(result);
-	});
+    // Send the result to the client
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
-app.get("/getVehNum", (req, res) => {
-  const userEmail = req.query.un; // Access the 'un' parameter from the query string
 
-  let sql = "SELECT vNum FROM users WHERE email=?";
-  con.query(sql, [userEmail], (err, result) => {
-    if (err) {
-      console.error("Database error:", err);
-      res.status(500).json({ error: "Database error" });
+app.get("/getVehNum", async (req, res) => {
+  try {
+    const userEmail = req.query.un; // Access the 'un' parameter from the query string
+    const sql = "SELECT vNum FROM users WHERE email=?";
+// Execute the query using await
+    const [result] = await con.execute(sql, [userEmail]);
+
+    // Close the connection
+    con.end();
+
+    if (result.length === 0) {
+      // If no results were found for the user's email
+      res.status(404).json({ message: "No data found for this user" });
     } else {
-      if (result.length === 0) {
-        // If no results were found for the user's email
-        res.status(404).json({ message: "No data found for this user" });
-      } else {
-        // If results were found, send them as a JSON response
-        res.status(200).json(result);
-      }
+      // If results were found, send them as a JSON response
+      res.status(200).json(result);
     }
-  });
-});
-
-
-app.delete("/delUser", (req, res) => {
-	let data = [req.body.vNum]
-	let sql = "delete from users where vNum=?";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
-});
-
-app.post("/savePUC", (req, res) => {
-	let data = [req.body.selectedValue, req.body.startDate, req.body.endDate];
-	let sql = "insert into puc (vNum, startDate, endDate) values(?, ?, ?)";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
-});
-
-app.get("/readPUC", (req, res) => {
-  const selectedValue = req.query.selectedValue;
-
-  if (!selectedValue) {
-    return res.status(400).json({ error: 'Select Vehicle Number' });
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500).json({ error: "Database error" });
   }
-
-  const sql = "SELECT * FROM puc WHERE vNum=?";
-  con.query(sql, [selectedValue], (err, result) => {
-    if (err) {
-      console.error('Error executing the SQL query: ' + err.message);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
-    }
-    res.json(result);
-  });
 });
 
 
-app.delete("/delPUC", (req, res) => {
-	let data = [ req.body.startDate ]
-	let sql = "delete from puc where startDate=?";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
-});
+app.delete("/delUser", async (req, res) => {
+  try {
+    const data = [req.body.vNum];
+    const sql = "DELETE FROM users WHERE vNum=?";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, data);
 
-app.post("/saveInsurance", (req, res) => {
-	let data = [req.body.selectedValue, req.body.policyNum, req.body.startDate, req.body.endDate, req.body.amount];
-	let sql = "insert into insurance (vNum, policyNum, startDate, endDate, amount) values(?, ?, ?, ?, ?)";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
-});
+    // Close the connection
+    con.end();
 
-app.get("/readInsurance", (req, res) => {
-  const selectedValue = req.query.selectedValue;
-
-  if (!selectedValue) {
-    return res.status(400).json({ error: 'Select Vehicle Number' });
+    // Send the result to the client
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
   }
-
-  const sql = "SELECT * FROM insurance WHERE vNum=?";
-  con.query(sql, [selectedValue], (err, result) => {
-    if (err) {
-      console.error('Error executing the SQL query: ' + err.message);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
-    }
-    res.json(result);
-  });
 });
 
-app.delete("/delInsurance", (req, res) => {
-	let data = [ req.body.startDate ]
-	let sql = "delete from insurance where startDate=?";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
-});
+app.post("/savePUC", async (req, res) => {
+  try {
+    const data = [req.body.selectedValue, req.body.startDate, req.body.endDate];
+    const sql = "INSERT INTO puc (vNum, startDate, endDate) VALUES (?, ?, ?)";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, data);
 
-app.post("/savePetrol", (req, res) => {
-	let data = [req.body.selectedValue, req.body.petrolDate, req.body.prevRead, req.body.curntRead, req.body.amount, req.body.litre ];
-	let sql = "insert into petrol (vNum, petrolDate, prevRead, curntRead, amount, litre) values(?, ?, ?, ?, ?, ?)";
-	con.query(sql, data, (err, result) => {
-		if (err)	res.send(err);
-		else		res.send(result);
-	});
-});
+    // Close the connection
+    con.end();
 
-app.get("/readPetrol", (req, res) => {
-  const selectedValue = req.query.selectedValue;
-
-  if (!selectedValue) {
-    return res.status(400).json({ error: 'Select Vehicle Number' });
+    // Send the result to the client
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
   }
-
-  const sql = "SELECT * FROM petrol WHERE vNum=?";
-  con.query(sql, [selectedValue], (err, result) => {
-    if (err) {
-      console.error('Error executing the SQL query: ' + err.message);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
-    }
-    res.json(result);
-  });
 });
 
-app.get("/getPrevRead", (req, res) => {
-  const selectedValue = req.query.selectedValue;
 
-  if (!selectedValue) {
-    return res.status(400).json({ error: 'Select Vehicle Number' });
+app.get("/readPUC", async (req, res) => {
+  try {
+    const selectedValue = req.query.selectedValue;
+
+    if (!selectedValue) {
+      return res.status(400).json({ error: 'Select Vehicle Number' });
+    }
+
+    const sql = "SELECT * FROM puc WHERE vNum=?";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, [selectedValue]);
+
+    // Close the connection
+    con.end();
+
+    res.json(result);
+  } catch (err) {
+    console.error('Error executing the SQL query: ' + err.message);
+    res.status(500).json({ error: 'Internal server error' });
   }
+});
 
-  const sql = "SELECT prevRead FROM petrol WHERE vNum=? ORDER BY uid DESC LIMIT 1";
-  con.query(sql, [selectedValue], (err, result) => {
-    if (err) {
-      console.error('Error executing the SQL query: ' + err.message);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
+
+app.delete("/delPUC", async (req, res) => {
+  try {
+    const data = [req.body.startDate];
+    const sql = "DELETE FROM puc WHERE startDate=?";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, data);
+
+    // Close the connection
+    con.end();
+
+    // Send the result to the client
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+});
+
+app.post("/saveInsurance", async (req, res) => {
+  try {
+    const data = [req.body.selectedValue, req.body.policyNum, req.body.startDate, req.body.endDate, req.body.amount];
+    const sql = "INSERT INTO insurance (vNum, policyNum, startDate, endDate, amount) VALUES (?, ?, ?, ?, ?)";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, data);
+
+    // Close the connection
+    con.end();
+
+    // Send the result to the client
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+});
+
+app.get("/readInsurance", async (req, res) => {
+  try {
+    const selectedValue = req.query.selectedValue;
+
+    if (!selectedValue) {
+      return res.status(400).json({ error: 'Select Vehicle Number' });
     }
+
+    const sql = "SELECT * FROM insurance WHERE vNum=?";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, [selectedValue]);
+
+    // Close the connection
+    con.end();
+
     res.json(result);
-  });
+  } catch (err) {
+    console.error('Error executing the SQL query: ' + err.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.delete("/delInsurance", async (req, res) => {
+  try {
+    const data = [req.body.startDate];
+    const sql = "DELETE FROM insurance WHERE startDate=?";
+	  // Execute the query using await
+    const [result] = await con.execute(sql, data);
+
+    // Close the connection
+    con.end();
+
+    // Send the result to the client
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
 
 
-app.listen(9000, () => { console.log("Ready to server @ 9000") });
+app.listen(9000, () => { 
+	console.log("Ready to server @ 9000") 
+});
 
 
 
